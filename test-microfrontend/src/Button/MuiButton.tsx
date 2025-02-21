06a3b00e4091b51abc91dab0f5c1834a3cf6d702
Siddant Rachha <siddant.rachha@gmail.com>
2025-02-22 02:25:07 +0530

    Initial Commit

---
+++ test-microfrontend/src/Button/MuiButton.tsx
@@ -0,0 +1,6 @@
+import React from 'react';
+import Button from '@mui/material/Button';
+
+export const MuiButton = () => {
+  return <Button variant="contained">Hello world</Button>;
+}
---
+++ test-surface/src/app/page.tsx
@@ -0,0 +1,6 @@
+import * from 'test-microfrontend';
+export default function Home() {
+  return (
+    <div>Test Surface</div>
+  );
+}
